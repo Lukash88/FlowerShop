de39fe44fdb5504fbbce5e5f5d199f6e3b338b13
Lukash88 <kocyq88@gmail.com>
2022-11-20 20:26:27 +0100

    pagination added to reservation

--- FlowerShop/FlowerShop.ApplicationServices/API/Domain/Reservation/GetReservationsResponse.cs
+++ FlowerShop/FlowerShop.ApplicationServices/API/Domain/Reservation/GetReservationsResponse.cs
@@ -1,9 +1,8 @@
 ﻿namespace FlowerShop.ApplicationServices.API.Domain.Reservation
 {
     using FlowerShop.ApplicationServices.API.Domain.Models;
-    using System.Collections.Generic;
 
-    public class GetReservationsResponse : ResponseBase<List<ReservationDTO>>
+    public class GetReservationsResponse : ResponseBase<PagedResponse<ReservationDTO>>
     {
     }
 }
