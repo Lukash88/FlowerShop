c3dafc90c80f357179548f5979854f9f1ceabdf6
Lukash88 <kocyq88@gmail.com>
2022-12-11 23:08:14 +0100

    added carousel to the home page with images

--- FlowerShop/client/src/app/shared/shared.module.ts
+++ FlowerShop/client/src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { CarouselModule } from 'ngx-bootstrap/carousel'
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 import { PagerComponent } from './components/pager/pager.component'
-
-
 
 @NgModule({
   declarations: [
@@ -13,12 +12,14 @@ import { PagerComponent } from './components/pager/pager.component'
   ],
   imports: [
     CommonModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports: [
     PaginationModule,
     PagingHeaderComponent,
-    PagerComponent
+    PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
