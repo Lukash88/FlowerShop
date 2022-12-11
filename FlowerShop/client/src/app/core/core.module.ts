5386899d0346c8aa4e28c19bef077c8218208b3d
Lukash88 <kocyq88@gmail.com>
2022-12-11 02:44:32 +0100

    add more styling to product-item.component

--- FlowerShop/client/src/app/core/core.module.ts
+++ FlowerShop/client/src/app/core/core.module.ts
@@ -5,26 +5,6 @@ import { RouterModule } from '@angular/router';
 import { BreadcrumbModule } from 'xng-breadcrumb'
 import { SectionHeaderComponent } from './section-header/section-header.component';
 
-
-
-// @NgModule({
-//   declarations: [
-//     NavBarComponent, 
-//     SectionHeaderComponent
-//   ],
-//   imports: [
-//     CommonModule,
-//     RouterModule,
-//     BreadcrumbModule
-//   ],
-//   exports: [
-//     NavBarComponent,
-//     SectionHeaderComponent
-//   ]
-// })
-// export class CoreModule { }
-
-
 @NgModule({
   declarations: [
     NavBarComponent, 
