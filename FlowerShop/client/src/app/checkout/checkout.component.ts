7ff17e2b98a043c90e67353469cd9663a73e4246
Lukash88 <kocyq88@gmail.com>
2023-02-04 22:19:26 +0100

    added checkout on client-side

---
+++ FlowerShop/client/src/app/checkout/checkout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.scss']
+})
+export class CheckoutComponent { }
