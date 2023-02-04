e8ee60ddeeea49c44c8df2795339e44a57289989
Lukash88 <kocyq88@gmail.com>
2023-02-04 22:05:07 +0100

    fixed some issues in navbar, section-header, pager, paging-header

--- FlowerShop/client/src/app/shared/components/paging-header/paging-header.component.ts
+++ FlowerShop/client/src/app/shared/components/paging-header/paging-header.component.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-paging-header',
   templateUrl: './paging-header.component.html',
   styleUrls: ['./paging-header.component.scss']
 })
-export class PagingHeaderComponent implements OnInit {
-  @Input() pageNumber: number;
-  @Input() pageSize: number;
-  @Input() totalCount: number;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PagingHeaderComponent {
+  @Input() pageNumber?: number;
+  @Input() pageSize?: number;
+  @Input() totalCount?: number;
 }
