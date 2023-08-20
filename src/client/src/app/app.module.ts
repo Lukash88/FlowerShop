833d922d8977ff864afae8226ad93b1b14167a07
Lukash88 <kocyq88@gmail.com>
2023-08-20 19:23:01 +0200

    Add JWT interceptor to client

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptors';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { LoadingInterceptor } from './core/interceptors/loading.interceptors';
     NgxSpinnerModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
