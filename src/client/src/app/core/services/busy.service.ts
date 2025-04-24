acf0c142fb19b617838d7fd0220a318f4daa1990
Lukash88 <kocyq88@gmail.com>
2025-04-24 22:54:57 +0200

    feature(webfrontend):  Replace Bootstrap and Bootswatch with Material Design theming andTailwind CSS

--- src/client/src/app/core/services/busy.service.ts
+++ src/client/src/app/core/services/busy.service.ts
@@ -1,28 +1,22 @@
 import { Injectable } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusyService {
+  loading = false;
   busyRequestCount = 0;
-
-  constructor(private spinnerService: NgxSpinnerService) { }
 
   busy() {
     this.busyRequestCount++;
-    this.spinnerService.show(undefined, {
-      type: 'ball-fussion',
-      bdColor: 'rgba(240,21,208,0.80)',
-      color: '#fec6ee'
-    });
+    this.loading = true;
   }
 
   idle() {
     this.busyRequestCount--;
     if (this.busyRequestCount <= 0) {
       this.busyRequestCount = 0;
-      this.spinnerService.hide();
+      this.loading = false;
     }
   }
 }
