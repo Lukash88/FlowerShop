df0b5a4ebcd60942ad5c2ab5a0cf19515187c56c
Lukash88 <kocyq88@gmail.com>
2025-04-29 22:43:08 +0200

    feat(webfrontend): Add MatDialog for filtering categories

---
+++ src/client/src/app/features/shop/filters-dialog/filters-dialog.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDivider } from '@angular/material/divider';
+import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { ShopService } from 'src/app/core/services/shop.service';
+
+@Component({
+  selector: 'app-filters-dialog',
+  standalone: true,
+  imports: [
+    MatDivider,
+    MatSelectionList,
+    MatListOption,
+    MatButton,
+    FormsModule,
+    CommonModule,
+    MatSelectionList
+  ],
+  templateUrl: './filters-dialog.component.html',
+  styleUrl: './filters-dialog.component.scss'
+})
+export class FiltersDialogComponent {
+  private dialogRef = inject(MatDialogRef<FiltersDialogComponent>);
+  shopService = inject(ShopService);
+  data = inject(MAT_DIALOG_DATA);
+
+  selectedCategories: string[] = this.data.selectedCategories;
+
+  onSelectionChange(event: MatSelectionListChange) {
+    const selectedOptions = event.options;
+    if (selectedOptions && selectedOptions.length > 0) {
+      this.selectedCategories = Array.from(selectedOptions.values()).map(option => option.value);
+    }
+    console.log('Selected Categories:', this.selectedCategories);
+
+    // Additional logic can be applied here if needed.
+  }
+
+  applyFilters() {
+    this.dialogRef.close({
+      selectedCategories: this.selectedCategories
+    });
+  }
+}
--- src/client/src/app/features/shop/shop.component.ts
+++ src/client/src/app/features/shop/shop.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatMenuTrigger } from '@angular/material/menu';
 import { ShopService } from 'src/app/core/services/shop.service';
-import { CategoryEnum } from 'src/app/shared/models/category';
 import { Product } from 'src/app/shared/models/product';
-import { ShopParams } from 'src/app/shared/models/shopParams';
+import { PageSizeOptions, ShopParams, SortOptions } from 'src/app/shared/models/shopParams';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { Pagination } from 'src/app/shared/models/pagination';
+import { MatSelectionListChange } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon-module.d-BeibE7j0';
+import { MatDialog } from '@angular/material/dialog.d-Dvsbu-0E';
+import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
+
 @Component({
     selector: 'app-shop',
     standalone: true,
@@ -15,26 +20,23 @@ import { Pagination } from 'src/app/shared/models/pagination';
       CommonModule,
       FormsModule,
       MatPaginator,
-      ProductItemComponent
+      MatIcon,
+      MatMenuTrigger,
+      ProductItemComponent,
+      FiltersDialogComponent
     ],
     templateUrl: './shop.component.html',
     styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
   @ViewChild('search', { static: false }) searchTerm: ElementRef;  
-  products?: Pagination<Product>; 
-  categories = CategoryEnum;  
+  products?: Pagination<Product>;
   shopParams = new ShopParams();
   totalCount = 0;
-  sortOptions = [
-    { name: 'Alphabetical', value: 'name' },
-    { name: 'Unalphabetical', value: '-name' },
-    { name: 'Price: Low to High', value: 'price' },
-    { name: 'Price: High to Low', value: '-price' }    
-  ];
-  pageSizeOptions = [5, 10, 15, 20];
+  sortOptions = SortOptions;
+  pageSizeOptions = PageSizeOptions;
 
-  constructor(private shopService: ShopService) { }
+  constructor(private shopService: ShopService, private dialogService: MatDialog) { }
 
   ngOnInit(): void {
     this.getProducts();
@@ -50,12 +52,35 @@ export class ShopComponent implements OnInit {
       },
       error: error => console.log(error)
     })
-  }  
+  }
+
+  openFiltersDialog() {
+    const dialogRef = this.dialogService.open(FiltersDialogComponent, {
+      minWidth: '400px',
+      data: {
+        selectedCategories: this.shopParams.categories
+      }
+    });
+    dialogRef.afterClosed().subscribe({
+      next: result => {
+        if (result) {
+          this.shopParams.categories = result.selectedCategories;
+          this.shopParams.pageNumber = 1;
+          console.log('Shop params: =>');
+          console.log(this.shopParams.categories);
+          this.getProducts();          
+        }
+      }
+    })
+  }
   
-  onCategorySelected(categoryName: CategoryEnum): void {
-    this.shopParams.categories = categoryName;
-    this.shopParams.pageNumber = 1;
-    this.getProducts();
+  onCategorySelected(event: MatSelectionListChange) {
+    const selectedOptions = event.options;
+    if (selectedOptions && selectedOptions.length > 0) {
+      this.shopParams.categories = Array.from(selectedOptions.values())
+        .map(option => option.value);
+      this.getProducts();
+    }
   }
 
   onSortSelected(sort: string) {
