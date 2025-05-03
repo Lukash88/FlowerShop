ba5909359c424b8320b5fa0e966079eb7da76265
Lukash88 <kocyq88@gmail.com>
2025-05-03 14:07:23 +0200

    Refactor(webfrontend): Replace SectionHeader with Header component

--- src/client/src/app/app.component.ts
+++ src/client/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from './core/services/cart.service';
 import { AccountService } from './core/services/account.service';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
+    imports: [
+      NavBarComponent,
+      HeaderComponent,
+      RouterModule
+    ],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']    
 })
