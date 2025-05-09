1b7892074b552cebfbbb48ee0c66e55f81ebac91
Lukash88 <kocyq88@gmail.com>
2025-05-10 00:00:56 +0200

    ref(webfrontend): Refactor CartService to use Angular Signals

--- src/client/src/app/core/services/cart.service.ts
+++ src/client/src/app/core/services/cart.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Cart, CartItem, CartTotals } from 'src/app/shared/models/cart';
+import { firstValueFrom } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { Cart, CartItem } from 'src/app/shared/models/cart';
 import { Product } from 'src/app/shared/models/product';
 import { DeliveryMethod } from 'src/app/shared/models/deliveryMethod';
 
@@ -11,101 +11,103 @@ import { DeliveryMethod } from 'src/app/shared/models/deliveryMethod';
   providedIn: 'root',
 })
 export class CartService {
-  baseUrl = environment.apiUrl;
-  private cartSource = new BehaviorSubject<Cart | null>(null);
-  cartSource$ = this.cartSource.asObservable();
-  private cartTotalSource = new BehaviorSubject<CartTotals | null>(null);
-  cartTotalSource$ = this.cartTotalSource.asObservable();
-  shippingPrice = 0.00;
+  baseUrl = environment.apiUrl;  
+  private http = inject(HttpClient);
+  cart = signal<Cart | null>(null);
+  itemCount = computed(() => {
+    return this.cart()?.items.reduce((sum, item) => sum + item.quantity, 0)
+  });
+  selectedDelivery = signal<DeliveryMethod | null>(null);
+  totals = computed(() => {
+    const cart = this.cart();
+    const delivery = this.selectedDelivery();
 
-  constructor(private http: HttpClient) { }
+    if (!cart) return null;
+    const subtotal = cart.items.reduce((sum, item) => sum + item.price * item.quantity, 0);      
+    const tax = subtotal * 0.08;
+    const shipping = delivery ? delivery.price : 0;
+
+    return {
+      subtotal,
+      shipping,
+      tax,
+      total: subtotal + shipping
+    }
+  });
 
   createPaymentIntent() {
-    return this.http.post<Cart>(this.baseUrl + 'payments/' + this.getCurrentCartValue()?.id, { })
+    return this.http.post<Cart>(this.baseUrl + 'payments/' + this.cart()?.id, { })
     .pipe(
       map((cart: any) => {
         cart = cart.data;
         console.log(cart);
-        this.cartSource.next(cart);
+        return cart;
       })
     );
   }
 
   getCart(id: string) {
-    return this.http.get<Cart>(this.baseUrl + 'cart/' + id).subscribe({
-      next: (cart: any) => {
+    return this.http.get<Cart>(this.baseUrl + 'cart/' + id).pipe(
+      map((cart: any) => {
         cart = cart.data;
-        this.cartSource.next(cart);
-        this.calculateTotals();
-      },
-    });
+        this.cart.set(cart);
+        return cart;
+      })
+    )
   }
 
   setCart(cart: Cart) {
-    return this.http.post<Cart>(this.baseUrl + 'cart/' + cart.id, cart).subscribe({
-      next: (cart: any) => {
+    return this.http.post<Cart>(this.baseUrl + 'cart/' + cart.id, cart).pipe(
+      tap((cart: any)=> {
         cart = cart.data;
-        this.cartSource.next(cart);
-        this.calculateTotals();
-      },
-    });
+        this.cart.set(cart)
+      })
+    )
   }
 
-  getCurrentCartValue() {
-    return this.cartSource.value;
-  }
-
-  addItemToCart(item: Product | CartItem, quantity = 1) {
-    if (this.isProduct(item)) item = this.mapProductItemToCartItem(item);
-    const cart = this.getCurrentCartValue() ?? this.createCart();
+  async addItemToCart(item: CartItem | Product, quantity = 1) {
+    const cart = this.cart() ?? this.createCart();
+    if (this.isProduct(item)) {
+      item = this.mapProductToCartItem(item);
+    }
     cart.items = this.addOrUpdateItem(cart.items, item, quantity);
-    this.setCart(cart);
+    await firstValueFrom(this.setCart(cart));
   }
 
-  removeItemFromCart(id: number, quantity = 1) {
-    const cart = this.getCurrentCartValue();
+  async removeItemFromCart(productId: number, quantity = 1) {
+    const cart = this.cart();
     if (!cart) return;
-    const item = cart.items.find((x) => x.productId === id);
-    if (item) {
-      item.quantity -= quantity;
-      if (item.quantity === 0) {
-        cart.items = cart.items.filter((x) => x.productId !== id);
+    const index = cart.items.findIndex(x => x.productId === productId);
+    if (index !== -1) {
+      if (cart.items[index].quantity > quantity) {
+        cart.items[index].quantity -= quantity;
+      } else {
+        cart.items.splice(index, 1);
       }
-      if (cart.items.length > 0) this.setCart(cart);
-      else this.deleteCart(cart);
+      if (cart.items.length === 0) {
+        this.deleteCart();
+      } else {
+        await firstValueFrom(this.setCart(cart));
+      }
     }
   }
 
-  deleteCart(cart: Cart) {    
-    return this.http.delete(this.baseUrl + 'cart/' + cart.id).subscribe({
+  deleteCart() {    
+    return this.http.delete(this.baseUrl + 'cart/' + this.cart()?.id).subscribe({
       next: () => {
-        this.deleteLocalCart();
-        this.shippingPrice = 0.00;
+        localStorage.removeItem('cart_id');
+        this.cart.set(null);
       }
     });
   }
 
-  deleteLocalCart() {
-    this.cartSource.next(null);
-    this.cartTotalSource.next(null);
-    localStorage.removeItem('cart_id');
-  }
-
-  private calculateTotals() {
-    const cart = this.getCurrentCartValue();
-    if (!cart) return;
-    const subtotal = cart.items.reduce((a, b) => (b.price * b.quantity) + a, 0);
-    const tax = subtotal * 0.08;
-    const total = subtotal + this.shippingPrice;
-    this.cartTotalSource.next({ shipping: this.shippingPrice, subtotal, tax, total });
-  }
-
-  private addOrUpdateItem(items: CartItem[], itemToAdd: CartItem, quantity: number): CartItem[] {
-    const item = items.find(x => x.productId === itemToAdd.productId);
-    if (item) item.quantity += quantity;
-    else {
-      itemToAdd.quantity = quantity;
-      items.push(itemToAdd);
+  private addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
+    const index = items.findIndex(x => x.productId === item.productId);
+    if (index === -1) {
+      item.quantity = quantity;
+      items.push(item);
+    } else {
+      items[index].quantity += quantity
     }
     return items;
   }
@@ -116,7 +118,7 @@ export class CartService {
     return cart;
   }
 
-  private mapProductItemToCartItem(item: Product): CartItem {
+  private mapProductToCartItem(item: Product): CartItem {
     return {
       productId: item.id,
       productName: item.name,
@@ -130,14 +132,5 @@ export class CartService {
 
   private isProduct(item: Product | CartItem): item is Product {
     return (item as Product).longDescription !== undefined;
-  }
-
-  setShippingPrice(deliveryMethod: DeliveryMethod) {
-    const cart = this.getCurrentCartValue(); 
-    if (deliveryMethod && cart) {
-      cart.deliveryMethodId = deliveryMethod.id;
-      this.shippingPrice = deliveryMethod.price;
-      this.setCart(cart);
-    }    
   }
 }
