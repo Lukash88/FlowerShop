e36763167a7e2849bd85cc958a61418b5e451701
Lukash88 <kocyq88@gmail.com>
2025-06-15 13:31:31 +0200

    Ref(webfrontend): Refactor text-input to use Angular Material components

--- src/client/src/app/shared/components/text-input/text-input.component.ts
+++ src/client/src/app/shared/components/text-input/text-input.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Self } from '@angular/core';
-import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
     selector: 'app-text-input',
+    standalone: true,
+    imports: [
+      ReactiveFormsModule,
+      CommonModule,
+      MatFormField,
+      MatInput,
+      MatError,
+      MatLabel
+    ],
     templateUrl: './text-input.component.html',
-    styleUrls: ['./text-input.component.scss'],
-    standalone: false
+    styleUrls: ['./text-input.component.scss']
 })
 export class TextInputComponent implements ControlValueAccessor {
   @Input() type = 'text';
