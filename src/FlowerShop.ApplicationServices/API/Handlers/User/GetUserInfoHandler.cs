6bf2a9be252e5042da30e0948f6ad55127c5443d
Lukash88 <kocyq88@gmail.com>
2025-06-23 22:33:41 +0200

    feat(webbackend): Add GetUserInfo feature

---
+++ src/FlowerShop.ApplicationServices/API/Handlers/User/GetUserInfoHandler.cs
@@ -0,0 +1,53 @@
+﻿using FlowerShop.ApplicationServices.API.Domain.User;
+using FlowerShop.ApplicationServices.API.Domain;
+using FlowerShop.ApplicationServices.API.ErrorHandling;
+using FlowerShop.DataAccess.Core.Entities.Identity;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using FlowerShop.ApplicationServices.API.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+
+namespace FlowerShop.ApplicationServices.API.Handlers.User;
+
+public class GetUserInfoHandler(IMapper mapper, UserManager<AppUser> userManager)
+    : IRequestHandler<GetUserInfoRequest, GetUserInfoResponse>
+{
+    public async Task<GetUserInfoResponse> Handle(GetUserInfoRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var user = await userManager.Users
+                .Include(u => u.Address)
+                .FirstOrDefaultAsync(u => u.Email == request.Email, cancellationToken);
+
+            if (user is null)
+            {
+                return new GetUserInfoResponse
+                {
+                    Error = new ErrorModel(ErrorType.NotFound)
+                };
+
+                //return new GetUserInfoResponse
+                //{
+                //   Data = null
+                //};
+            }
+
+            var userInfoDto = mapper.Map<UserInfoDto>(user);
+
+            return new GetUserInfoResponse
+            {
+                Data = userInfoDto
+            };
+        }
+        catch (Exception ex)
+        {
+            // TODO: Log the exception
+            return new GetUserInfoResponse
+            {
+                Error = new ErrorModel($"{ErrorType.Unauthorized} - Unexpected error: {ex.Message}")
+            };
+        }
+    }
+}
