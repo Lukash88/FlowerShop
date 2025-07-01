b517941e04f508040c81a7ff3c179e15f5e06d42
Lukash88 <kocyq88@gmail.com>
2025-07-01 22:22:11 +0200

    ref(webfrontend): Refactor app initialization and add HTTP interceptors

--- src/client/src/app/app.config.ts
+++ src/client/src/app/app.config.ts
@@ -4,22 +4,25 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { lastValueFrom } from 'rxjs';
 import { InitService } from './core/services/init.service';
-
-function initializeApp() {
-  const initService = inject(InitService); 
-  return lastValueFrom(initService.init()).finally(() => {
-    const splash = document.getElementById('initial-splash');
-    if (splash) {
-      splash.remove(); 
-    }
-  });
-}
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideAppInitializer(initializeApp)
+    provideHttpClient(withInterceptors([
+      loadingInterceptor,
+      jwtInterceptor
+    ])),
+    provideAppInitializer(() => {
+      const initService = inject(InitService);
+      return lastValueFrom(initService.init()).finally(() => {
+        const splash = document.getElementById('initial-splash');
+        if (splash) splash.remove();
+      });
+    })
   ]
 };
