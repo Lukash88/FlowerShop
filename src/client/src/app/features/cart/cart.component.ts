bea64970bd01acc2e9a428712e1e0d875a1782d5
Lukash88 <kocyq88@gmail.com>
2025-07-17 22:43:13 +0200

    ref(webfrontend): Refactor cart component for improved modularity

--- src/client/src/app/features/cart/cart.component.ts
+++ src/client/src/app/features/cart/cart.component.ts
@@ -1,18 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CartService } from 'src/app/core/services/cart.service';
-import { CartItem } from 'src/app/shared/models/cart';
-import { CommonModule } from '@angular/common';
-import { CartSummaryComponent } from 'src/app/shared/components/cart-summary/cart-summary.component';
+import { CartItemComponent } from './cart-item/cart-item.component';
 import { OrderTotalsComponent } from 'src/app/shared/components/order-totals/order-totals.component';
-import { EmptyStateComponent } from 'src/app/shared/components/empty-state/empty-state.component';
 import { Router } from '@angular/router';
+import { EmptyStateComponent } from 'src/app/shared/components/empty-state/empty-state.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [    
-    CommonModule,
-    CartSummaryComponent,
+  imports: [
+    CartItemComponent,
     OrderTotalsComponent,
     EmptyStateComponent
   ],
@@ -20,15 +17,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent {
-  constructor(public cartService: CartService, private router: Router) {}  
-
-  incrementQuantity(item: CartItem) {
-    this.cartService.addItemToCart(item);
-  }
-
-  removeItem(event: {id: number, quantity: number}) {
-    this.cartService.removeItemFromCart(event.id, event.quantity);
-  }
+  private router = inject(Router);
+  cartService = inject(CartService);
 
   onAction() {
     this.router.navigateByUrl('/shop');
