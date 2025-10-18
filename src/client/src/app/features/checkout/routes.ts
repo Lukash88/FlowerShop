ee49fb8ab3973d3c0802d6de950e9e4634778ba9
Lukash88 <kocyq88@gmail.com>
2025-10-18 15:27:43 +0200

    feat(webfrontend): Add orderCompleteGuard for checkout success route

--- src/client/src/app/features/checkout/routes.ts
+++ src/client/src/app/features/checkout/routes.ts
@@ -3,8 +3,9 @@ import { CheckoutComponent } from "./checkout.component";
 import { CheckoutSuccessComponent } from "./checkout-success/checkout-success.component";
 import { authGuard } from "src/app/core/guards/auth.guard";
 import { emptyCartGuard } from "src/app/core/guards/empty-cart.guard";
+import { orderCompleteGuard } from "src/app/core/guards/order-complete.guard";
 
 export const checkoutRoutes: Route[] = [
   { path: '', component: CheckoutComponent, canActivate: [authGuard, emptyCartGuard] },
-  { path: 'success', component: CheckoutSuccessComponent, canActivate: [authGuard] }
+  { path: 'success', component: CheckoutSuccessComponent, canActivate: [authGuard, orderCompleteGuard] }
 ];
