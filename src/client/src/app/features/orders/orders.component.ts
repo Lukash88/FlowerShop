576a188fb76aec61ee586706b509020fc2924b84
Lukash88 <kocyq88@gmail.com>
2025-10-25 08:32:45 +0200

    feat(webfrontend): Redesign orders pages with Material components and enhanced UX

--- src/client/src/app/features/orders/orders.component.ts
+++ src/client/src/app/features/orders/orders.component.ts
@@ -1,19 +1,26 @@
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { OrdersService } from 'src/app/core/services/order.service';
+import { RouterModule } from '@angular/router';
+import { OrderService } from 'src/app/core/services/order.service';
 import { Order, PaginationParams } from 'src/app/shared/models/order';
 
 @Component({
-    selector: 'app-orders',
-    standalone: true,
-    templateUrl: './orders.component.html',
-    styleUrls: ['./orders.component.scss']
+  selector: 'app-orders',
+  standalone: true,
+  imports: [
+    RouterModule,
+    DatePipe,
+    CurrencyPipe
+  ],
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss'],
 })
 export class OrdersComponent implements OnInit {
   orders: Order[] = [];
-  paginationParams = new PaginationParams;
+  paginationParams = new PaginationParams();
   totalCount = 0;
 
-  constructor(private orderService: OrdersService) { }
+  constructor(private orderService: OrderService) {}
 
   ngOnInit(): void {
     this.getOrders();
@@ -21,14 +28,14 @@ export class OrdersComponent implements OnInit {
 
   getOrders() {
     this.orderService.getOrdersForUser(this.paginationParams).subscribe({
-      next: (response: any) => { 
+      next: (response: any) => {
         this.orders = response.data.results;
         this.paginationParams.pageNumber = response.data.currentPage;
         this.paginationParams.pageSize = response.data.pageSize;
         this.totalCount = response.data.rowCount;
       },
-      error: error => console.log(error)
-    })
+      error: (error: any) => console.log(error.error),
+    });
   }
 
   onPageChanged(event: any) {
