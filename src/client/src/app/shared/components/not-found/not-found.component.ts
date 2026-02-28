754b122d3ada107ba7689567675dd6fca2587dec
Lukash88 <kocyq88@gmail.com>
2026-02-28 23:55:39 +0100

    feat(webfrontend): Add error handling components for 404 and 500 pages

---
+++ src/client/src/app/shared/components/not-found/not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [MatIcon, MatButton, RouterLink],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss',
+})
+export class NotFoundComponent {
+  @Input() title = '404';
+  @Input() message = 'Page not found';
+}
